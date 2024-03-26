db1fb54871401825fec6912513a87feee058ec2e
RichardRodi <RichardmRodi@gmail.com>
2024-03-26 14:53:30 -0400

    I finished all exercises.

--- app.js
+++ app.js
@@ -5,6 +5,38 @@ console.log(
 
 // Exercise 1
 
-// Exercise 2
+//1. Declare a variable named `firstName` using the `const` keyword
+//2. Declare a variable named `lastName` using the `let` keyword
+//3. Declare a variable named `age` using the `var` keyword
 
+// Exercise 2
+//1. Assign your first name as the **string** value to the variable `firstName`
+//2. Assign your last name as the **string** value to the variable `lastName`
+//3. Assign your age as the **number** value to the variable `age`
+
+const firstName = "Richard";
+let lastName = "Rodi";
+var age = "36";
+console.log (`Hello my name is ${firstName} ${lastName} and my age is ${36}`);
 // Exercise 3
+
+let language = "javaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log (`${language} was invented in ${createdYear}.`);
+
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+console.log (`The item is ${price} and it is on sale? ${isOnSale}. They are in Stock ${inStock} There are this many in Stock: ${stock} Its size is ${selectedSize}`)
+
+let title = "name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
+
+console.log (`The title of this book is ${title} and the author is ${author}, the page count is: ${pageCount}. Im book marked at page ${bookmark}. I have read this ${hasRead}`)
